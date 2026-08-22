b17ff0ba9d7408c4aa4781f7e7b593f36136b801
Yann D'Souza <yannssouza@outlook.com>
2026-08-22 13:25:48 -0300

    finish

--- parser/parse_config.py
+++ parser/parse_config.py
@@ -1,5 +1,19 @@
-def parse_config():
-    contents = ""
-    with open("config.txt", "r") as file:
-        contents = file.read()
-        print(contents)
+def parse_config() -> None:
+    data: dict[str, str] = {}
+
+    try:
+        with open("config.txt", "r", encoding="utf-8") as file:
+            for i, raw_line in enumerate(file, start=1):
+                line = raw_line.strip()
+
+                if not line or line.startswith("#"):
+                    continue
+
+                if "=" not in line:
+                    raise Exception(f"Invalid line format at line {i}.")
+
+                key, value = line.split("=", maxsplit=1)
+                data[key.strip()] = value.strip()
+            print(data)
+    except FileNotFoundError as error:
+        print(error)
