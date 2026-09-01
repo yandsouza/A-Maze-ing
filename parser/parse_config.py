49ef43aca9552d5b7e7fcaa05229954775fb5e0f
Yann D'Souza <yannssouza@outlook.com>
2026-09-01 14:57:31 -0300

    convert types finished

--- parser/parse_config.py
+++ parser/parse_config.py
@@ -1,5 +1,8 @@
+from typing import Any
+
+
 def parse_config(path: str) -> None:
-    data: dict[str, str] = {}
+    config: dict[str, str] = {}
 
     try:
         with open(path, "r", encoding="utf-8") as file:
@@ -13,7 +16,42 @@ def parse_config(path: str) -> None:
                     raise Exception(f"Invalid input at line {i}.")
 
                 key, value = line.split("=", maxsplit=1)
-                data[key.strip()] = value.strip()
-            print(data)
+                config[key.strip()] = value.strip()
+            convert_data(config)
     except FileNotFoundError as error:
         print(error)
+
+
+def convert_data(config: dict[str, str]) -> dict[str, Any]:
+    safe_config: dict[str, Any] = {}
+
+    try:
+        safe_config["width"] = int(config["WIDTH"])
+    except Exception as error:
+        print(error)
+
+    try:
+        safe_config["height"] = int(config["HEIGHT"])
+    except Exception as error:
+        print(error)
+
+    try:
+        x, y = config["ENTRY"].split(",", maxsplit=1)
+        safe_config["entry"] = (int(x), int(y))
+    except Exception as error:
+        print(error)
+
+    try:
+        x, y = config["EXIT"].split(",", maxsplit=1)
+        safe_config["exit"] = (int(x), int(y))
+    except Exception as error:
+        print(error)
+
+    safe_config["output_file"] = config["OUTPUT_FILE"]
+
+    if config["PERFECT"] == "True":
+        safe_config["perfect"] = True
+    else:
+        safe_config["perfect"] = False
+
+    return safe_config
